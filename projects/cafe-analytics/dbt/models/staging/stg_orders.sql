99f02a9085c12b2d2bb2e6514b3cc119a064524a
Guo Yi Tang <gytang26@gmail.com>
2024-03-25 08:30:08 +0000

    make changes in staging models

--- projects/cafe-analytics/dbt/models/staging/stg_orders.sql
+++ projects/cafe-analytics/dbt/models/staging/stg_orders.sql
@@ -1,10 +1,19 @@
--- cleaning and preparing the order data
+-- extract and preparing data in the staging layer from BigQuery
+with
+    raw_orders as (
+        select
+            order_id,
+            customer_id,
+            ip_addr,
+            cast(date_created as timestamp) as date_created,  -- Casting to TIMESTAMP if not already
+            cast(date_paid as timestamp) as date_paid,  -- Casting to TIMESTAMP if not already
+            total,
+            status,
+            items as items_json  -- Directly using items as JSON-formatted string
+        from `jr-data-training.cafe.cafe-sales`  -- Your source table
+    )
+
 select
-    order_id,
-    customer_id,
-    ip_addr,
-    timestamp(date_created) as date_created_ts,
-    timestamp(date_paid) as date_paid_ts,
-    total,
-    status
-from `jr-data-training.cafe.cafe-sales`
+    order_id, customer_id, ip_addr, date_created, date_paid, total, status, items_json
+from raw_orders
+
