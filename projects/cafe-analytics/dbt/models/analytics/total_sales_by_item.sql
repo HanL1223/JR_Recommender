7289143fff58d94d2ea3f25a82278bb300505ac6
Clement <gytang26@gmail.com>
2024-04-01 03:04:21 +1030

    upload powerbi folder

--- projects/cafe-analytics/dbt/models/analytics/total_sales_by_item.sql
+++
@@ -1,6 +0,0 @@
--- total sales revenue
--- aggregate total sales by item, taking into account the adjusted item prices.
-select item_name, sum(item_price) as total_sales, count(order_id) as total_orders
-from {{ ref("item_details") }}
-group by item_name
-order by total_sales desc
