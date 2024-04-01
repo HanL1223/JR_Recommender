5f37460ec8b49bc74141a451470ce95a0ecb6428
clement tang <gytang26@gmail.com>
2024-04-01 03:57:14 +0000

    update opening hours normalised

--- projects/cafe-analytics/dbt/models/transformation/opening_hours_normalised.sql
+++ projects/cafe-analytics/dbt/models/transformation/opening_hours_normalised.sql
@@ -1,23 +1,14 @@
 with
     cte_opening_hours as (
         select
-            case
-                when date not like 'normal_%' then safe_cast(date as date) else null
-            end as actual_date,
-            case
-                when open_time != '' then safe_cast(open_time as int64) else null
-            end as open_time,
-            case
-                when close_time != '' then safe_cast(close_time as int64) else null
-            end as close_time,
+            actual_date,
+            open_time,
+            close_time,
             special_note,
-            case
-                when date like 'normal_%' then replace(date, 'normal_', '') else null
-            end as weekday,
-            case
-                when date like 'normal_%' then 'normal' else 'special'
-            end as day_category
-        from `{{ ref('stg_opening_hours') }}`
+            -- Convert 'day_category' directly, no need for case statement since
+            -- 'actual_date' and 'day_category' are already correctly aligned
+            day_category
+        from {{ ref("stg_opening_hours") }}
     ),
 
     cte_order_details_enriched as (
@@ -27,16 +18,13 @@ with
             oh.close_time as closing_time,
             oh.special_note,
             oh.day_category
-        from `{{ ref('order_details') }}` o
+        from {{ ref("order_details") }} o
         left join
             cte_opening_hours oh
-            on (
-                oh.day_category = 'special'
-                and format_date('%Y-%m-%d', o.date_created) = oh.actual_date
-            )
+            on (oh.day_category = 'special' and date(o.date_created) = oh.actual_date)
             or (
                 oh.day_category = 'normal'
-                and format_date('%A', o.date_created) = oh.weekday
+                and format_date('%A', o.date_created) = oh.special_note  -- Assuming 'special_note' field is used to store the weekday name for 'normal' days
             )
     )
 
