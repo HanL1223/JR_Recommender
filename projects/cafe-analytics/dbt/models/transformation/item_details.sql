c22fac7b63bc0c81872f3bcab34ad466d447cf30
clement tang <gytang26@gmail.com>
2024-04-02 15:22:10 +0000

    update item_details model, dense rank, item category hard coded

--- projects/cafe-analytics/dbt/models/transformation/item_details.sql
+++ projects/cafe-analytics/dbt/models/transformation/item_details.sql
@@ -1,33 +1,105 @@
--- extracting item details from the JSON array in BigQuery, with price and quantity
--- adjustment, including order time details, and adding a hashed ID
 select
-    farm_fingerprint(
-        concat(extracted.item_name, extracted.item_category)
-    ) as item_hash_id,
+    concat(
+        cast(extracted.order_id as string), '-', cast(extracted.item_rank as string)
+    ) as order_item_ranking,
     extracted.*
 from
     (
         select
             o.order_id,
-            -- Directly extracting item-level attributes
             cast(json_extract_scalar(item, '$.name') as string) as item_name,
-            cast(json_extract_scalar(item, '$.category') as string) as item_category,
-            -- Extracting the quantity, assuming 1 when null
+            case
+                when
+                    cast(json_extract_scalar(item, '$.name') as string) in (
+                        'Cappuccino',
+                        'Latte',
+                        'Espresso',
+                        'White Hot Chocolate',
+                        'Short Black',
+                        'Piccolo',
+                        'Mocha',
+                        'Long Macchiato',
+                        'Long Black',
+                        'Hot Chocolate',
+                        'Golden Latte',
+                        'Short Macchiato',
+                        'Magic',
+                        'Macha Latte',
+                        'Flat White',
+                        'Dirty Chai Latte',
+                        'Chai Latte',
+                        'Babychino'
+                    )
+                then 'Hot Drinks'
+                when
+                    cast(json_extract_scalar(item, '$.name') as string) in (
+                        'Croissant',
+                        'Toasted Roll',
+                        'Quiche',
+                        'Orange Almond Cake (VG)',
+                        'Orange Almond Cake (GF,VG)',
+                        'Homemade Sausage Roll',
+                        'Toastie',
+                        'Toasties',
+                        'Plain Toast',
+                        'Quiche',
+                        'Homemade Spinach Ricotta Roll',
+                        'Homemade Sausage Roll',
+                        'Hash Brown',
+                        'Fruit Toast (V)',
+                        'Fruit Toast',
+                        'Crepe',
+                        'Chips and Wedges'
+                    )
+                then 'Food'
+                when
+                    cast(json_extract_scalar(item, '$.name') as string) in (
+                        'Iced Coffee',
+                        'Smoothie',
+                        'Spider',
+                        'Milkshake (Syrup)',
+                        'Thickshake (Syrup)',
+                        'Muffin',
+                        'Orange Almond Cake (GF,VG)',
+                        'Scone',
+                        'Iced Mocha with Cream',
+                        'Iced Coffee with Cream',
+                        'Iced Chocolate with Cream',
+                        'Ice Latte',
+                        'Ice Chai',
+                        'Friand (gf)',
+                        'Friand',
+                        'Freshly Squeezed Juice',
+                        'Focaccia',
+                        'Drinks from fridge',
+                        'Cookies',
+                        'Coffee Milkshake',
+                        'Coffee Frappe',
+                        'Carrot Cake',
+                        'Brownie',
+                        'Banana Bread',
+                        'Affogato',
+                        'Acai smoothie bowl (vo)'
+                    )
+                then 'Cold Drinks'
+                else 'Kitchen'
+            end as item_category,
             ifnull(
                 safe_cast(json_extract_scalar(item, '$.quantity') as int64), 1
             ) as item_quantity,
-            -- Adjusting the price from cents to dollars and rounding to 2 decimal
-            -- places
             round(
                 cast(json_extract_scalar(item, '$.price') as float64) / 100, 2
             ) as item_price,
-            -- Extracting order time details
             cast(
                 json_extract_scalar(o.items_json, '$.order_time') as string
             ) as order_time,
             cast(
                 json_extract_scalar(o.items_json, '$.order_time_verbose') as string
-            ) as order_time_verbose
+            ) as order_time_verbose,
+            dense_rank() over (
+                partition by o.order_id
+                order by cast(json_extract_scalar(item, '$.name') as string)
+            ) as item_rank
         from {{ ref("stg_orders") }} as o
         cross join unnest(json_extract_array(o.items_json, '$.cart')) as item
     ) as extracted
