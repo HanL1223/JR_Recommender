667e9f9a9464dca12117607b50144fd6e82c5bb6
MaxineXiong <maxinexiong2@gmail.com>
2024-10-16 03:11:19 +1100

    fix(data-model): paid_order_details: item_tracking_id moved before item

--- projects/cafe-analytics/data-model/models/analytics/paid_order_details.sql
+++ projects/cafe-analytics/data-model/models/analytics/paid_order_details.sql
@@ -1,9 +1,9 @@
 SELECT DISTINCT od.order_id, od.customer_id,
                 od.cart_total_price AS order_price,
+                id.item_id AS item_tracking_id,
                 id.item_name AS item,
                 id.item_category AS category,
                 id.item_quantity AS quantity, id.item_price,
-                id.item_id AS item_tracking_id,
                 od.date_created AS order_time
 FROM `jr-data-training.dbt_cafeanalytics.order_details` AS od
 JOIN `jr-data-training.dbt_cafeanalytics.item_details` AS id
