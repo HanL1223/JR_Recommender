e408d707dfbeab952f0e8eda8339249190cfb64c
HanL1223 <han.li1223@outlook.com>
2025-11-23 20:13:07 +1100

    update model script

--- cafe_analytics_202511/models/silver/int_order_item_options.sql
+++ cafe_analytics_202511/models/silver/int_order_item_options.sql
@@ -3,46 +3,85 @@
     schema='silver'
 ) }}
 
-
 WITH parsed_cart AS (
-  SELECT
-    order_id,
-    customer_id,
-    ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY cart_item) AS order_item_id, -- unique per cart row
-     DATE(TIMESTAMP(date_created)) AS order_date,
-    TIME(TIMESTAMP(date_created)) AS order_time,
-    cart_item,
-    CAST(JSON_VALUE(items, '$.cart_total_price') AS INT64) AS order_total_price,
-    CAST(JSON_VALUE(items, '$.cart_gst') AS NUMERIC) AS order_gst,
-    CAST(JSON_VALUE(items, '$.cart_surcharge') AS INT64) AS order_surcharge,
-    LTRIM(JSON_VALUE(items, '$.cart_total_price_display'), '$') AS order_display_price,
-    LTRIM(JSON_VALUE(items, '$.cart_gst_display'), '$') AS order_display_gst,
-    JSON_VALUE(items, '$.cart_size') AS cart_size,
-    JSON_VALUE(items, '$.order_time') AS cart_order_time
-FROM {{ ref('lnd_orders') }},
-  UNNEST(JSON_EXTRACT_ARRAY(items, '$.cart')) AS cart_item
+    SELECT
+        order_id,
+        customer_id,
+        ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY cart_item) AS order_item_id,
+        DATE(TIMESTAMP(date_created)) AS order_date,
+        TIME(TIMESTAMP(date_created)) AS order_time,
+        cart_item,
+        CAST(JSON_VALUE(items, '$.cart_total_price') AS INT64) AS order_total_price,
+        CAST(JSON_VALUE(items, '$.cart_gst') AS NUMERIC) AS order_gst,
+        CAST(JSON_VALUE(items, '$.cart_surcharge') AS INT64) AS order_surcharge,
+        LTRIM(JSON_VALUE(items, '$.cart_total_price_display'), '$') AS order_display_price,
+        LTRIM(JSON_VALUE(items, '$.cart_gst_display'), '$') AS order_display_gst,
+        JSON_VALUE(items, '$.cart_size') AS cart_size,
+        JSON_VALUE(items, '$.order_time') AS cart_order_time
+    FROM {{ ref('lnd_orders') }},
+         UNNEST(JSON_EXTRACT_ARRAY(items, '$.cart')) AS cart_item
 )
+
 SELECT
-  order_id,
-  order_item_id,
-  customer_id,
-  JSON_VALUE(cart_item, '$.name') AS product_name,
-  JSON_VALUE(cart_item, '$.variant_name') AS product_variant,
-  JSON_VALUE(cart_item, '$.category') AS product_category,
-  CAST(JSON_VALUE(cart_item, '$.price') AS INT64) AS product_total_price,
-  JSON_VALUE(option, '$.name') AS product_option_name,
-  JSON_VALUE(option, '$.value') AS product_option_value,
-  CAST(JSON_VALUE(option, '$.price') AS INT64) AS product_option_price,
- order_date,
-  order_time,
-  order_total_price,
-  order_gst,
-  order_surcharge,
-  order_display_price,
-  order_display_gst,
-  cart_size,
-  cart_order_time
-  --
+    order_id,
+    order_item_id,
+    customer_id,
+
+    ----------------------
+    -- PRODUCT FIELDS
+    ----------------------
+    JSON_VALUE(cart_item, '$.name') AS product_name,
+    JSON_VALUE(cart_item, '$.variant_name') AS product_variant,
+
+    /* ------------------------------
+       CORRECTED CATEGORY MAPPING
+       ------------------------------ */
+    CASE
+        
+        /* Hot Drinks */
+        WHEN JSON_VALUE(cart_item, '$.name') IN (
+            'White Hot Chocolate', 'Short Macchiato', 'Piccolo', 'Mocha',
+            'Magic', 'Long Macchiato', 'Long Black', 'Latte', 'Hot Chocolate',
+            'Golden Latte', 'Flat White', 'Espresso', 'Dirty Chai Latte',
+            'Chai Latte', 'Cappuccino', 'Babychino'
+        ) THEN 'Hot Drinks'
+
+        /* Cold Drinks */
+        WHEN JSON_VALUE(cart_item, '$.name') IN (
+            'Thickshake (Syrup)', 'Smoothie', 'Milkshake (Syrup)',
+            'Iced Mocha with Cream', 'Iced Coffee with Cream',
+            'Iced Chocolate with Cream', 'Ice Latte', 'Ice Chai',
+            'Freshly Squeezed Juice', 'Affogato', 'Coffee Milkshake',
+            'Coffee Frappe', 'Drinks from fridge'
+        ) THEN 'Cold Drinks'
+        
+        /* Everything else is Kitchen */
+        ELSE 'Kitchen'
+    END AS product_category,
+
+    CAST(JSON_VALUE(cart_item, '$.price') AS INT64) AS product_total_price,
+
+    ----------------------
+    -- PRODUCT OPTIONS
+    ----------------------
+    JSON_VALUE(option, '$.name') AS product_option_name,
+    JSON_VALUE(option, '$.value') AS product_option_value,
+    CAST(JSON_VALUE(option, '$.price') AS INT64) AS product_option_price,
+
+    ----------------------
+    -- ORDER FIELDS
+    ----------------------
+    order_date,
+    order_time,
+    order_total_price,
+    order_gst,
+    order_surcharge,
+    order_display_price,
+    order_display_gst,
+    cart_size,
+    cart_order_time
+
 FROM parsed_cart,
-  UNNEST(JSON_EXTRACT_ARRAY(cart_item, '$.options')) AS option
+     UNNEST(JSON_EXTRACT_ARRAY(cart_item, '$.options')) AS option
+
 ORDER BY order_id, order_item_id
--- cafe_analytics_202511/models/silver/int_orders_item.sql
+++ cafe_analytics_202511/models/silver/int_orders_item.sql
@@ -3,83 +3,64 @@
     schema='silver'
 ) }}
 
--- WITH items_base AS (
---   SELECT  
---     order_id,
---     position + 1 AS line_item_number,
---     CAST(JSON_VALUE(item, '$.name') AS STRING) AS item_name,
---     CAST(JSON_VALUE(item, '$.variant_name') AS STRING) AS variant_name,
---     CAST(JSON_VALUE(item, '$.category') AS STRING) AS category,
---     CAST(JSON_VALUE(item, '$.variant_desc') AS STRING) AS variant_desc,
---     CAST(JSON_VALUE(item, '$.variant_image') AS STRING) AS variant_image,
---     CAST(JSON_VALUE(item, '$.unitprice') AS INT64) AS unit_price,
---     CAST(JSON_VALUE(item, '$.price') AS INT64) AS line_item_total,
---     COALESCE(CAST(JSON_VALUE(item, '$.quantity') AS INT64), 1) AS quantity,
---     {{ dbt_utils.generate_surrogate_key(['order_id', 'position']) }} AS order_item_sk,
---     CURRENT_TIMESTAMP() AS loaded_at
---   FROM {{ ref('lnd_orders') }} AS src,
---   UNNEST(JSON_EXTRACT_ARRAY(items, '$.cart')) AS item WITH OFFSET AS position
--- ),
-
--- options AS (
---   SELECT  
---     {{ dbt_utils.generate_surrogate_key(['order_id', 'position']) }} AS order_item_sk,
---     CAST(JSON_VALUE(option, '$.name') AS STRING) AS option_name,
---     CAST(JSON_VALUE(option, '$.value') AS STRING) AS option_value,
---     CAST(JSON_VALUE(option, '$.price') AS INT64) AS option_price
---   FROM {{ ref('lnd_orders') }} AS src,
---   UNNEST(JSON_EXTRACT_ARRAY(items, '$.cart')) AS item WITH OFFSET AS position,
---   UNNEST(JSON_EXTRACT_ARRAY(item, '$.options')) AS option
--- )
-
--- SELECT
---   ib.*,
---   o.option_name,
---   o.option_value,
---   o.option_price,
---   COALESCE(o.option_price, 0) AS total_options_price,
---   ib.unit_price - COALESCE(o.option_price, 0) AS base_variant_price,
-
---   {{ dbt_utils.generate_surrogate_key(['ib.item_name', 'ib.variant_name']) }} AS product_base_key,
---   {{ dbt_utils.generate_surrogate_key(['ib.item_name', 'ib.variant_name', 'o.option_name', 'o.option_value']) }} AS product_option_key
-
--- FROM items_base ib
--- LEFT JOIN options o
---   ON ib.order_item_sk = o.order_item_sk
-{{ config(
-    materialized='view',
-    schema='silver'
-) }}
-
-WITH parsed_cart AS (
-  SELECT
-    order_id,
-    customer_id,
-    ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY cart_item) AS order_item_id, -- unique per cart row
-      DATE(TIMESTAMP(date_created)) AS order_date,
-    TIME(TIMESTAMP(date_created)) AS order_time,
-    cart_item,
-    CAST(JSON_VALUE(items, '$.cart_total_price') AS INT64) AS order_total_price,
-    CAST(JSON_VALUE(items, '$.cart_gst') AS NUMERIC) AS order_gst,
-    CAST(JSON_VALUE(items, '$.cart_surcharge') AS INT64) AS order_surcharge,
-    LTRIM(JSON_VALUE(items, '$.cart_total_price_display'), '$') AS order_display_price,
-    LTRIM(JSON_VALUE(items, '$.cart_gst_display'), '$') AS order_display_gst,
-    JSON_VALUE(items, '$.cart_size') AS cart_size,
-    JSON_VALUE(items, '$.order_time') AS cart_order_time,
-  FROM {{ ref('lnd_orders') }},
-  UNNEST(JSON_EXTRACT_ARRAY(items, '$.cart')) AS cart_item
-)
+WITH parsed_cart AS (SELECT order_id,
+                            customer_id,
+                            ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY cart_item) AS order_item_id, -- unique per cart row
+                            DATE(TIMESTAMP(date_created))                                AS order_date,
+                            TIME(TIMESTAMP(date_created))                                AS order_time,
+                            cart_item,
+                            CAST(JSON_VALUE(items, '$.cart_total_price') AS INT64)       AS order_total_price,
+                            CAST(JSON_VALUE(items, '$.cart_gst') AS NUMERIC)             AS order_gst,
+                            CAST(JSON_VALUE(items, '$.cart_surcharge') AS INT64)         AS order_surcharge,
+                            LTRIM(JSON_VALUE(items, '$.cart_total_price_display'), '$')  AS order_display_price,
+                            LTRIM(JSON_VALUE(items, '$.cart_gst_display'), '$')          AS order_display_gst,
+                            JSON_VALUE(items, '$.cart_size')                             AS cart_size,
+                            JSON_VALUE(items, '$.order_time')                            AS cart_order_time,
+                     FROM {{ ref('lnd_orders') }},
+                          UNNEST (JSON_EXTRACT_ARRAY(items, '$.cart')) AS cart_item
+    )
 SELECT
-  order_id,
-  order_item_id,
-  customer_id,
- order_date,
- order_time,
-    order_total_price,order_gst,order_surcharge,order_display_price,order_display_gst,cart_size,cart_order_time,
-  JSON_VALUE(cart_item, '$.name') AS product_name,
-  JSON_VALUE(cart_item, '$.variant_name') AS product_variant,
-  JSON_VALUE(cart_item, '$.category') AS product_category,
-  CAST(JSON_VALUE(cart_item, '$.price') AS INT64) AS product_total_price,
+    order_id,
+       order_item_id,
+       customer_id,
+       order_date,
+       order_time,
+       order_total_price,
+       order_gst,
+       order_surcharge,
+       order_display_price,
+       order_display_gst,
+       cart_size,
+       cart_order_time,
+       JSON_VALUE(cart_item, '$.name')                 AS product_name,
+       JSON_VALUE(cart_item, '$.variant_name')         AS product_variant,
+       CAST(JSON_VALUE(cart_item, '$.price') AS INT64) AS product_total_price,
+       CASE
+
+        /* Hot Drinks */
+        WHEN JSON_VALUE(cart_item, '$.name') IN (
+            'White Hot Chocolate', 'Short Macchiato', 'Piccolo', 'Mocha',
+            'Magic', 'Long Macchiato', 'Long Black', 'Latte', 'Hot Chocolate',
+            'Golden Latte', 'Flat White', 'Espresso', 'Dirty Chai Latte',
+            'Chai Latte', 'Cappuccino', 'Babychino'
+        ) THEN 'Hot Drinks'
+
+        /* Cold Drinks */
+        WHEN JSON_VALUE(cart_item, '$.name') IN (
+            'Thickshake (Syrup)', 'Smoothie', 'Milkshake (Syrup)',
+            'Iced Mocha with Cream', 'Iced Coffee with Cream',
+            'Iced Chocolate with Cream', 'Ice Latte', 'Ice Chai',
+            'Freshly Squeezed Juice', 'Affogato', 'Coffee Milkshake',
+            'Coffee Frappe', 'Drinks from fridge'
+        ) THEN 'Cold Drinks'
+
+        /* Everything else is Kitchen */
+        ELSE 'Kitchen'
+
+    END AS product_category
 
 FROM parsed_cart
+where 0 = 0
+  and JSON_VALUE(cart_item, '$.category') is not null
 ORDER BY order_id, order_item_id
+
